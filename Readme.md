adf80c3ce60d68ceb14602a5fc4e737c5bf836e1
Kyuwoong Lee <kwooler@gmail.com>
2022-10-19 01:05:57 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+
+[Link to Chap 5 Lab10](https://docs.google.com/presentation/d/1r3h2R9JwK9HK_U2Ia-zncL0BSjHV6Giu6ugNJ6yZpgc/edit#slide=id.g16b637da4c8_3_224)
+
+![Lab2](https://nimbus-screenshots.s3.amazonaws.com/s/d5cedc6b2b734e31bb75cfda881e5da3.png)
+
+## Compete the "main.py"
+
+
---
+++ main.py
@@ -0,0 +1,25 @@
+import random 
+
+
+def merge(number1, number2):
+	#################
+	# Make your code 
+	#################
+	return mlist
+
+
+number1 = [0, 2, 3]
+number2 = [1, 4, 5, 6, 9]
+retlist = merge(number1, number2)
+print (retlist)
+# #########################################
+
+n1 = [ random.randint(0,20) for i in range(5)]
+n2 = [ random.randint(0,20) for i in range(3)]
+n1.sort()
+n2.sort()
+print (n1)
+print (n2)
+retlist = merge(n1, n2)
+print (retlist)
+
