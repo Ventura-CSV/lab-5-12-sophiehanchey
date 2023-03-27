463882e485e5a2beba69a2558736b0f18b7b2461
Kyu Lee <kwooler@gmail.com>
2023-03-27 01:32:41 -0700

    L512

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import random
 
+
 def split(numbers):
-	#################
-	# Make your code 
-	#################
-	return numbers
+    #################
+    # Make your code
+    #################
+
+    #################
+    # Do not delete return statement
+    return numbers
 
 
-numbers = [3,2,0,5,4]
-# print (id(numbers))
-numbers = split(numbers)
-# print (id(numbers))
-print (numbers)
+def main():
+    numbers = [3, 2, 0, 5, 4]
+    # print (id(numbers))
+    numbers = split(numbers)
+    # print (id(numbers))
+    print(numbers)
+
+    numbers = [random.randint(0, 20) for i in range(10)]
+    print(numbers)
+    numbers = split(numbers)
+    print(numbers)
 
 
-numbers = [ random.randint(0,20) for i in range(10)]
-print (numbers)
-numbers = split(numbers)
-print (numbers)
+if __name__ == '__main__':
+    main()
